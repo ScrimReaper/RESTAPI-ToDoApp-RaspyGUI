23512070690aa703580bd730d1bc3aba4d5dc901
Moritz Adelsberger <e12322880@student.tuwien.ac.at>
2024-12-11 00:44:05 +0100

    starting to redo my file structure, defining namespaces,...

---
+++ API/constants/JsonF.h
@@ -0,0 +1,20 @@
+//
+// Created by User on 11.12.2024.
+//
+
+#ifndef JSONFIELDS_H
+#define JSONFIELDS_H
+namespace JsonF {
+    namespace task {
+        constexpr auto ID = "id";
+        constexpr auto NAME = "name";
+        constexpr auto DONE = "done";
+    }
+
+    namespace list {
+        constexpr auto ID = "id";
+        constexpr auto NAME = "name";
+        constexpr auto TASKS = "tasks";
+    }
+}
+#endif //JSONFIELDS_H
