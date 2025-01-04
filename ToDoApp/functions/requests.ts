7bc08c74634f98968836581c11826d89899f9dfe
Adi <moritz.adelsberger@gmail.com>
2025-01-04 21:32:04 +0100

    added all the other fetch functions, gonna implement them in the UI now

--- ToDoApp/functions/requests.ts
+++ ToDoApp/functions/requests.ts
@@ -4,13 +4,21 @@ import {List, Task} from "@/app/types";
 
 //this function returns the lists with their IDs as Key Value Pairs
 export async function fetchList(): Promise<List[]> {
-    const response = await fetch(BASEURL, {
-        method: "GET",
-        headers: {
-            "API-KEY": "1234"
-        }
-    });
-    const data = await response.json();
+    let data;
+    try {
+        const response = await fetch(BASEURL, {
+            method: "GET",
+            headers: {
+                "API-KEY": "1234"
+            }
+        });
+        data = await response.json();
+
+
+    } catch (error) {
+        console.error("There was an Error fetching the Lists", error);
+        return [];
+    }
     const listArray: List[] = [];
     let tempList: List;
     for (const item of data) {
@@ -26,13 +34,21 @@ export async function fetchList(): Promise<List[]> {
 
 //this function returns the tasks of a specific List in a map with their ids as keys
 export async function fetchTasks(listId: number): Promise<Task[]> {
-    const response = await fetch(BASEURL + "/" + listId + "/tasks", {
-        method: "GET",
-        headers: {
-            "API-KEY": "1234"
-        }
-    });
-    const data = await response.json();
+    let data;
+
+    try {
+        const response = await fetch(BASEURL + "/" + listId + "/tasks", {
+            method: "GET",
+            headers: {
+                "API-KEY": "1234"
+            }
+        });
+        data = await response.json();
+    } catch (error) {
+        console.error("There was an Error fetching the tasks ", error);
+        return [];
+    }
+
     const tasks = data.tasks;
     if (!tasks) {
         return [];
@@ -47,6 +63,56 @@ export async function fetchTasks(listId: number): Promise<Task[]> {
         taskArray.push(tempTask);
     }
     return taskArray;
+}
+
+async function postList(listName: string) {
+    try {
+        const response = await fetch(BASEURL, {
+            method: "POST",
+            headers: {
+                "API-KEY": "1234"
+            },
+            body: JSON.stringify({listName: listName}),
+        });
+        return response.status === 201 //if req was created return true
+    } catch (e) {
+        console.error("There was an Error posting the list" + e);
+        return false;
+    }
+
+
+}
+
+async function postTask(listId: number, taskBody:string) {
+    try {
+        const response = await fetch(BASEURL + "/" + listId + "/tasks", {
+            method: "POST",
+            headers: {
+                "API-KEY": "1234"
+            },
+            body: JSON.stringify({taskBody: taskBody}),
+        })
+        return response.status === 201
+    } catch (error) {
+        console.error("There was an Error posting the task", error);
+        return false;
+    }
+}
+
+async function deleteTask(listId: number, taskId: number) {
+    try {
+        const response = await fetch(BASEURL + "/" + listId + "/tasks" + taskId, {
+            method: "DELETE",
+            headers: {
+                "API-KEY": "1234"
+            }
+        })
+        return response.status === 204;
+    } catch (error) {
+        console.error("There was an Error deleteing the task" + error);
+        return false;
+    }
+
 }
 
 
