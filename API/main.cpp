888bfc2ec8b61040b1498eb9dbf2dfa06ce3e804
Adi <moritz.adelsberger@gmail.com>
2025-01-05 01:32:48 +0100

    some commets

--- API/main.cpp
+++ API/main.cpp
@@ -11,7 +11,7 @@ int main() {
     crow::App<Middleware, crow::CORSHandler> app;
     auto &cors = app.get_middleware<crow::CORSHandler>();
     cors.global()
-            .origin("*") // Allow all origins for development (replace with your frontend origin in production)
+            .origin("*") // Allow all origins for development; TODO:Change them when deploying
             .methods(crow::HTTPMethod::GET, crow::HTTPMethod::POST, crow::HTTPMethod::PUT, crow::HTTPMethod::Delete,
                      crow::HTTPMethod::OPTIONS)
             .headers("Content-Type, Authorization, API-KEY");
@@ -21,4 +21,5 @@ int main() {
     Routes::setUpRoutes(app, listManager);
 
     app.port(18080).run();
+
 }
