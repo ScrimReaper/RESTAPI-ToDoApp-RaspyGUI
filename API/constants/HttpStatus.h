e464050548e64704f882a06fa494b44b232bc66f
MoMentum <166304133+ScrimReaper@users.noreply.github.com>
2025-08-27 22:49:40 +0200

    feat(api): modularize HttpStatus constants to seperate .h

---
+++ API/constants/HttpStatus.h
@@ -0,0 +1,14 @@
+//
+// Created by User on 11.12.2024.
+//
+
+#ifndef HTTPSTATUS_H
+#define HTTPSTATUS_H
+namespace HttpStatus{
+    constexpr int OK = 200;
+    constexpr int CREATED = 201;
+    constexpr int NOCONTENT = 204;
+    constexpr int BADREQUEST = 400;
+    constexpr int NOTFOUND = 404;
+}
+#endif //HTTPSTATUS_H
